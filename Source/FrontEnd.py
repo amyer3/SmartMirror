84e7f6023bef8d189e249110e2ec928004577b99
amyer3 <aj.myers93@gmail.com>
2017-08-29 22:24:34 -0700

    Smart Mirror Initial

--- Source/FrontEnd.py
+++ Source/FrontEnd.py
@@ -1,16 +1,31 @@
+import Source.MirrorData as md
+import PyQt5
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 
-from PyQt5.QtWidgets import QApplication, QWidget
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        lbl1 = QLabel(md.stocks(), self)
+        lbl1.move(15, 10)
+
+        lbl2 = QLabel('tutorials', self)
+        lbl2.move(35, 40)
+
+        lbl3 = QLabel('for programmers', self)
+        lbl3.move(55, 70)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Absolute')
+        self.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(500, 500)
-    w.move(300, 300)
-    w.setWindowTitle('Sample Window for Mirror')
-    w.show()
-
+    ex = MainWindow()
     sys.exit(app.exec_())
