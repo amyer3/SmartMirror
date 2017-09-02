a7b1a78eb7cf4c86265c7d276037393ef804b9e1
amyer3 <aj.myers93@gmail.com>
2017-09-01 17:48:15 -0700

    Smart Mirror 9/1

--- Source/AppJar.py
+++ Source/AppJar.py
@@ -2,9 +2,20 @@ import Source.MirrorData as md
 from appJar import *
 
 
+newsarr = md.news()
 app = gui()
-app.addLabel("l1", "Simple Demo")
-app.addEntry("text")
-app.addButton("OK")
-app.addEmptyLabel("l2")
+app.setBg("black")
+#app.setExpand("both")
+
+app.setSticky("ne")
+app.addLabel("wthr", md.weather(), 0, 2)
+
+app.setSticky("nw")
+app.addLabel("time", md.times(), 0, 0, 1)
+
+app.setSticky("s")
+app.addLabel("news", newsarr[1], 4, 1, 1)
+
+app.setFg("white")
+app.setGeometry("fullscreen")
 app.go()
