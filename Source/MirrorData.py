42ebbea35803bbba2f34c07617307a93d9848e2f
Alex <alexander.j.myers@icloud.com>
2017-09-11 23:37:35 -0700

    Smart Mirror 9/11

--- Source/MirrorData.py
+++ Source/MirrorData.py
@@ -35,38 +35,17 @@ def news():
     return hlns
 
 
-def stocks():
-    url = 'https://www.quandl.com/api/v3/datasets/CHRIS/CME_ES1.json?rows=1&api_key='+keys.keyFinder('stocks')
-    c = urlopen(url)
-    json_parsed = json.loads(c.read().decode('UTF-8'))
-    price = json_parsed['dataset']['data'][0][4]
-    return "S&P500: $%s" % price
-
-
 def times():
     dtfmt = '%A' + os.linesep + '%B %d, %Y' + os.linesep + '%I:%M:%S %p'
     date = datetime.datetime.now(pytz.timezone('US/Pacific')).strftime(dtfmt)
     return date
 
 
-def strFormatter():
-    tup = weather()
-    colBreak = ": "
-    hyphBreak = " - "
-    barBreak = " | "
-    arr = []
-    for i in range(1, 5):
-        out = tup[3][i] + colBreak + tup[2][i] + hyphBreak + tup[0][i] + barBreak + tup[1][i]
-        arr.insert(i, out)
-    return arr
-    #print(arr[0])
-
-
 def svgSelector():
     inpt = weather()
     cond = inpt[2][0]
     if 'overcast' or 'cloudy' in cond:
-        return 'icons/mostly-cloudy.svg'
+        return 'icons/overcast.svg'
     elif 'rain' or 'showers' or 'drizzle' in cond:
         return 'icons/rain.svg'
     elif 'snow' or 'ice' or 'hail' in cond:
---
+++ Source/Voice_Control.py
@@ -0,0 +1,8 @@
+from threading import Thread
+
+
+class main(Thread):
+    def __init__(self):
+        super().__init__()
+
+
