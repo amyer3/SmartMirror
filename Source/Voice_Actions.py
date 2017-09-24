2f73dd34ee4720f84b0ce22aac129df7472eebf2
Alex <alexander.j.myers@icloud.com>
2017-09-23 21:05:00 -0700

    begin voice module buildout

--- Source/Voice_Actions.py
+++ Source/Voice_Actions.py
@@ -1,15 +1,21 @@
 import speech_recognition as sr
+import pyaudio
 import evernote as en
 
 
-
-def master(string):
+def master():
+    with sr.Microphone() as source:
+        print("Jasper is listening")
+        audio = sr.Recognizer().listen(source)
+        string = sr.Recognizer().recognize_sphinx(audio)
     if "list" in string:
         addList(string)
     if "do math" in string:
-        mathTest(string)
+        mathTest()
+    if "reset" in string:
+        setBGN()
 
-def mathTest(string):
+def mathTest():
     print("2 + 2 = 4")
 
 
@@ -19,9 +25,15 @@ def addList(string):
     print(command)
     print(finder(words))
 
+
 def finder(arr):
     r = arr.index("to")
     return r
 
-def setBGN(string):
 
+def setBGN():
+    with sr.Microphone() as source:
+        sr.Recognizer().adjust_for_ambient_noise(source)
+    print("completed")
+
+    # TODO make jasper init with a 1s pause to adjust for ambient noise?
--- Source/Voice_Control.py
+++ Source/Voice_Control.py
@@ -1,19 +1,25 @@
 from threading import Thread
 import speech_recognition as sr
-import Voice_Actions as res
+import Voice_Actions as actions
 
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("say something")
-    audio = r.listen(source)
 
-try:
-    print(r.recognize_sphinx(audio))
-    res.master(r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("ukve")
-except sr.RequestError:
-    print("ReqE")
+def listen():
+    with sr.Microphone() as source:
+        print("say something")
+        audio = r.listen(source)
+    try:
+        rec = r.recognize_sphinx(audio)
+        print(rec)
+        if rec == "jasper":
+            actions.master()
+        else:
+            listen()
+    except sr.UnknownValueError:
+        print("unknown value error")
+    except sr.RequestError:
+        print("request error")
 
+listen()
